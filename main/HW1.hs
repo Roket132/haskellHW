ce7c4a5a1fb001f5d3ace6c2a06022d7d6232a1c
Laletin Dmitry <roket@yandex-team.ru>
2021-01-15 11:13:58 +0700

    fix few warnings

--- main/HW1.hs
+++ main/HW1.hs
@@ -1,4 +1,5 @@
-
+module HW1 where 
+    
 f1_1 :: Int -> Int
 f1_1 n = n
 
@@ -10,28 +11,39 @@ f2_1 n = let
               | a == 2 = True
               | a > 2 = (length [x | x <- [2 .. ceiling $ sqrt $ fromIntegral $ a - 1]
                         ,a `mod` x == 0]) == 0
+              | otherwise = False
     in isPrime $ abs n
 
 f3_1 :: Bool -> Bool -> Int
 f3_1 a b = let
-    fromEnum :: Bool -> Int
-    fromEnum f = if f == True then 1 else 0
-    in (+) (fromEnum a) (fromEnum b)
+    fromEnumS :: Bool -> Int
+    fromEnumS f = if f == True then 1 else 0
+    in (+) (fromEnumS a) (fromEnumS b)
 
 f4_1 :: Int -> Int
-f4_1 n = let
+f4_1 m = let
     helper :: Int -> Int -> Int -> Int
     helper acc n 1 = acc + 1
     helper acc n d = if mod n d == 0 
                         then helper ((acc + ) d + (div n d)) (n) (d - 1)
                         else helper acc n (d - 1)
-    in helper 0 n $ round $ sqrt $ fromIntegral $ abs n
+    in helper 0 m $ round $ sqrt $ fromIntegral $ abs m
 
 f5_1 :: Int -> Int
 f5_1 n = if (f4_1 $ n + 1) == n + 1 then n + 1 else f5_1(n + 1)
 
 f6_1 :: Integer -> Integer
-f6_1 n = if (f4_1 $ n + 1) == n + 1 then n + 1 else f6_1(n + 1)
+f6_1 n = if (f4_but_integer $ n + 1) == n + 1 then n + 1 else f6_1(n + 1)
 
 f12_1 :: Int
 f12_1 = (+) 1 1
+
+
+f4_but_integer :: Integer -> Integer
+f4_but_integer m = let
+    helper :: Integer -> Integer -> Integer -> Integer
+    helper acc n 1 = acc + 1
+    helper acc n d = if mod n d == 0 
+                        then helper ((acc + ) d + (div n d)) (n) (d - 1)
+                        else helper acc n (d - 1)
+    in helper 0 m $ round $ sqrt $ fromIntegral $ abs m
