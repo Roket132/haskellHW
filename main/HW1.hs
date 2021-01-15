fbeb053fd844cdddf9ab5a67150715d746f2386a
Laletin Dmitry <roket@yandex-team.ru>
2021-01-15 12:13:01 +0700

    add f10_1

--- main/HW1.hs
+++ main/HW1.hs
@@ -1,5 +1,5 @@
 module HW1 where 
-    
+
 f1_1 :: Int -> Int
 f1_1 n = n
 
@@ -35,9 +35,29 @@ f5_1 n = if (f4_1 $ n + 1) == n + 1 then n + 1 else f5_1(n + 1)
 f6_1 :: Integer -> Integer
 f6_1 n = if (f4_but_integer $ n + 1) == n + 1 then n + 1 else f6_1(n + 1)
 
+f7_1 :: Int -> Int -> Int
+f7_1 m n | m == 0 = (+) n 1
+         | m > 0 && n == 0 = f7_1 (m - 1) (1)
+         | m > 0 && n > 0 = f7_1 (m - 1) (f7_1 m $ n - 1)
+
+f8_1 :: Int -> Int -> Integer
+f8_1 m n | m == 0 = toInteger $ (+) n 1
+         | m > 0 && n == 0 = toInteger $ f7_1 (m - 1) (1)
+         | m > 0 && n > 0 = toInteger $ f7_1 (m - 1) (f7_1 m $ n - 1)
+
+f10_1 :: Double -> Double -> (Double, Double)
+f10_1 n m = let 
+    intPart :: Double -> Double -> Double
+    intPart a b | a > 0 && b > 0 = fromIntegral $ truncate $ (/) a b
+                | a < 0 && b > 0 = fromIntegral $ ((-1) *) $ ceiling $ ( / b) $ abs a
+                | a > 0 && b < 0 = fromIntegral $ ((-1) *) $ truncate $ (a / ) $ abs b
+                | a < 0 && b < 0 = fromIntegral $ ceiling $ ((/) $ abs a) $ abs b
+                | b == 0 = undefined
+                | otherwise = 0
+    in  (intPart n m, (n - ) $ (*) m $ intPart n m)
+
 f12_1 :: Int
 f12_1 = (+) 1 1
-
 
 f4_but_integer :: Integer -> Integer
 f4_but_integer m = let
