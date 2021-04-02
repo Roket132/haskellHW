0bb12960420aefe04e9bddc1c21bc8f34b7f6b7e
Laletin Dmitry <roket@yandex-team.ru>
2021-04-02 11:16:33 +0300

    add second variant

--- main/MergeSortParallel.hs
+++ main/MergeSortParallel.hs
@@ -7,13 +7,18 @@ import Data.List
 -- cabal install --lib parallel
 -- ghc -threaded MergeSortParallel.hs -rtsopts
 
+-- ghc -threaded -O3 -XBangPatterns MergeSortParallel.hs -rtsopts
+-- +RTS -N4 -s -RTS
+
+-- take 10 (merge_sort $ get_random_list 10000000 100 124325)
+
+-- feager-blackholing
 
 get_random_list :: Int -> Int -> Int -> [Int]
 get_random_list n m seed = map (mod m) $ take n $ random_list m seed
 
 random_list :: Int -> Int -> [Int]
 random_list m seed = randomRs (1, m) (mkStdGen seed)
-            
 
 merge_sort :: [Int] -> [Int]
 merge_sort list
@@ -27,41 +32,60 @@ merge_sort list
                         then l : (merge left (r : right))
                         else r : (merge (l : left) right)
             in
-                par sortLeft (pseq sortRight (merge sortLeft sortRight))
-        where 
-            left = take half list 
-            right = drop half list
-            half = (length list) `div` 2
+                merge sortLeft sortRight
+            where 
+                left = take half list 
+                right = drop half list
+                half = (length list) `div` 2
 
-            sortLeft = merge_sort left
-            sortRight = merge_sort right
+                sortLeft = merge_sort left
+                sortRight = merge_sort right 
 
 
-merge_sort_parallel :: (Show a, Ord a) => [a] -> [a]
-merge_sort_parallel list | length list == 1 = list
-                         | otherwise = 
-                            let
-                                merge :: Ord a => [a] -> [a] -> [a] -> [a]
-                                merge res [] [] = reverse res
-                                merge res left [] = reverse res ++ left
-                                merge res [] right = reverse res ++ right
-                                merge res (l : left) (r : right) = if l < r 
-                                    then merge (l : res) (left) (r : right)
-                                    else merge (r : res) (l : left) (right)
-                                
-                                split :: Ord a => [a] -> [a] -> [a] -> Int -> ([a], [a])
-                                split left right [] len = (left, right)
-                                split left right (x : list) len =
-                                    if length left < div len 2
-                                        then split (x : left) (right) (list) len
-                                        else split (left) (x : right) (list) len
-                            in
-                                let
-                                    pair = split [] [] list $ length list
-                                    sortLeft = merge_sort_parallel $ fst pair
-                                    sortRight = merge_sort_parallel $ snd pair
-                                in
-                                    par sortLeft (pseq sortRight (merge [] sortLeft sortRight))
+merge_sort_parallel1 :: [Int] -> [Int]
+merge_sort_parallel1 list
+        | length list < 2 = list
+        | otherwise = 
+            let 
+                merge :: [Int] -> [Int] -> [Int]
+                merge [] right = right
+                merge left [] = left
+                merge (l : left) (r : right) = if l < r
+                        then l : (merge left (r : right))
+                        else r : (merge (l : left) right)
+            in
+                sortLeft `par` sortRight `pseq` (merge sortLeft sortRight)
+            where 
+                left = take half list 
+                right = drop half list
+                half = (length list) `div` 2
+
+                sortLeft = merge_sort_parallel1 left
+                sortRight = merge_sort_parallel1 right
+
+
+merge_sort_parallel2 :: [Int] -> [Int]
+merge_sort_parallel2 list
+        | length list < 2 = list
+        | otherwise = 
+            let 
+                merge :: [Int] -> [Int] -> [Int]
+                merge [] right = right
+                merge left [] = left
+                merge (l : left) (r : right) = if l < r
+                        then l : (merge left (r : right))
+                        else r : (merge (l : left) right)
+            in
+                runEval $ do 
+                    !sortLeft <- rseq (merge_sort_parallel2 left)
+                    !sortRight <- rseq (merge_sort_parallel2 right)
+                    let !res = merge sortLeft sortRight
+                    return res
+
+                    where
+                        left = take  half list 
+                        right = drop half list
+                        half = (length list) `div` 2
                                 
 
 m = 1000
@@ -71,9 +95,12 @@ test n =
     let
         rndList = get_random_list n m seed
     in
-        merge_sort rndList
+        merge_sort_parallel2 rndList
 
-main = print$ take 10 $ test 10000000
+main :: IO ()
+main = do
+    !sorted <- print (take 10 (test 1000000))
+    return sorted
 
 -- for n = 50000000
 -- -N1   : 11s
